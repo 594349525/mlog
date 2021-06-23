873caec4e3a8189e9922ef825c9731eae8e02d57
liyk <494210279@qq.com>
2021-06-23 16:56:07 +0800

    d

--- test/testLogger.php
+++ test/testLogger.php
@@ -1,7 +1,11 @@
 <?php
 require_once "../vendor/autoload.php";
 
-$str = "xxxxxxx";
-$str1 = Xiangxin\Logger\MLog::TestWrite($str);
-var_dump($str1);
+use Xiangxin\Logger\MLog;
+
+MLog::init('/Users/liyk/56hello/Mlog/logs/', '', '', 45);
+
+MLog::info('测试内容5', [['name' => 'lyk'], ['code' => 'ddddd']], 'financial');
+MLog::error('测试内容6', [['name' => 'lyk'], ['code' => 'ddddd']], 'note');
+var_dump(1);
 die;
